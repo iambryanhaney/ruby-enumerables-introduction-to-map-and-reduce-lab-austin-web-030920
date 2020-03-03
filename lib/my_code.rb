cb2e847fc170db28a5a728221e10bcc0e4eaab55
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 01:44:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # My Code here....
+
+def map_to_negativize(source)
+  result = []
+  i = 0
+  while i < source.length do
+    result << source * -1
+    i += 1
+  end
+  result
+end
+
+def map_to_no_change(source)
+  result = []
+  i = 0
+  while i < source.length do
+    result << source * -1
+    i += 1
+  end
+  result
+end
