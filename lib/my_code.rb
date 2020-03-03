58a1d23b29500100fdd88c03896bcea4277f8625
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 01:46:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(source)
   result = []
   i = 0
   while i < source.length do
-    result << source * -1
+    result << source[i] * -1
     i += 1
   end
   result
@@ -14,12 +14,46 @@ def map_to_no_change(source)
   source
 end
 
-def map_to_negativize(source)
+def map_to_double(source)
   result = []
   i = 0
   while i < source.length do
-    result << source * -1
+    result << source[i] * 2
     i += 1
   end
   result
 end
+
+def map_to_square(source)
+  result = []
+  i = 0
+  while i < source.length do
+    result << source[i]**2
+    i += 1
+  end
+  result
+end
+
+def reduce_to_total(source)
+  total = 0
+  i = 0
+  while i < source.length do
+    total += source[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source)
+  total = 0
+  i = 0
+  while i < source.length do
+    total += source[i]
+    i += 1
+  end
+  total
+end
+
+
+
+
